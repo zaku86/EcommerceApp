8e7408c2544f88f6497e77c12fa7b79170f49377
Zaku <zakumokieva@gmail.com>
2024-04-18 11:46:29 -0400

    finished products API.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 let icon = document.querySelector(".icon");
 let mobileLinks = document.querySelector(".mobile-links");
 let showMenu = true;
+let productContainer = document.querySelector(".productContainer");
+
 icon.addEventListener("click", function () {
   if (showMenu === true) {
     mobileLinks.style.display = "block";
@@ -10,3 +12,37 @@ icon.addEventListener("click", function () {
     showMenu = true;
   }
 });
+
+getApi();
+
+function getApi() {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        let product = data[i];
+
+        productContainer.innerHTML += `
+          <div class="card">
+            <div class="cardImg">
+              <img src="${product.image}" alt="${product.title}" />
+            </div>
+            <div class="cardContent">
+              <h2>${product.title}</h2>
+              <p>${product.description.slice(0, 100) + "..."}</p>
+              
+              <button>Buy for $${product.price}</button>
+            </div>
+          </div>
+        `;
+      }
+    });
+}
+// TODO:Work with weather apis
+// persisting data in browser
+// work on cart page
+// work on single product page
+// work on footer
+// implement add to cart functionality
+// implement payment
